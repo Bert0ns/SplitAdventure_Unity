5e2c9f967fdf86f61b347267b72d69632ef482be
Bert0ns <121551954+Bert0ns@users.noreply.github.com>
2024-10-01 19:21:29 +0200

    multiplayer added (local)

--- Assets/Scripts/Character/CharacterInputManager.cs
+++ Assets/Scripts/Character/CharacterInputManager.cs
@@ -4,23 +4,35 @@ using UnityEngine.InputSystem;
 
 public class CharacterInputManager : MonoBehaviour
 {
-    [SerializeField] private InputActionReference moveAction;
-    [SerializeField] private InputActionReference jumpAction;
-    [SerializeField] private InputActionReference handsUpAction;
-    [SerializeField] private InputActionReference handsDownAction;
     [SerializeField] private GameObject character;
     [SerializeField] private float stickDeadZone = 0.2f;
 
     private CharacterMovement characterMovement;
+    private InputActionAsset inputAsset;
+    private InputActionMap playerMap;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+    private InputAction handsUpAction;
+    private InputAction handsDownAction;
+
+    private void Awake()
+    {
+        inputAsset = this.GetComponent<PlayerInput>().actions;
+        playerMap = inputAsset.FindActionMap("Player");
+    }
 
     void Start()
     {
         characterMovement = character.GetComponent<CharacterMovement>();
+        moveAction = playerMap.FindAction(playerMap.actions[0].name);
+        jumpAction = playerMap.FindAction(playerMap.actions[1].name);
+        handsUpAction = playerMap.FindAction(playerMap.actions[2].name);
+        handsDownAction = playerMap.FindAction(playerMap.actions[3].name);
     }
 
     void Update()
     {
-        Vector2 moveDirection = moveAction.action.ReadValue<Vector2>();
+        Vector2 moveDirection = moveAction.ReadValue<Vector2>();
         if (moveDirection.x > 0 + stickDeadZone)
         {
             characterMovement.CharacterMoveRight();
@@ -34,18 +46,18 @@ public class CharacterInputManager : MonoBehaviour
             characterMovement.CharacterMoveIdle();
         }
 
-        bool isTryingToJump = jumpAction.action.triggered;
+        bool isTryingToJump = jumpAction.triggered;
         if(isTryingToJump)
         {
             characterMovement.CharacterTryJump();
         }
 
-        float handsUp = handsUpAction.action.ReadValue<float>();
+        float handsUp = handsUpAction.ReadValue<float>();
         if (handsUp > 0)
         {
             characterMovement.CharacterMoveHandsUp();
         }
-        float handsDown = handsDownAction.action.ReadValue<float>();
+        float handsDown = handsDownAction.ReadValue<float>();
         if (handsDown > 0)
         {
             characterMovement.CharacterMoveHandsDown();
