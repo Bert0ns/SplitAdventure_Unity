51d5694e349903ee8843ca45c3f04980a1c11243
Davide Bertoni <121551954+Bert0ns@users.noreply.github.com>
2024-10-04 12:37:24 +0200

    update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
-using UnityEngine.Events;
 
 [RequireComponent(typeof(PlayerManager))]
 public class GameManager : MonoBehaviour
